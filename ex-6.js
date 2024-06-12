11b12720ab07006c68c13dcc2a7fdb9db7be17d4
Por <Poramaporn2540@gmail.com>
2024-06-12 18:03:36 +0700

    feat(exercise): add complete exercises

--- ex-6.js
+++ ex-6.js
@@ -3,5 +3,8 @@ let companyName = "TechUp Thailand";
 let reversedCompanyName = "";
 
 // Start coding here
-
+for (let i = 1; i < companyName.length; i = i+1) {
+    reversedCompanyName += companyName[companyName.length-i]
+    //console.log(reversedCompanyName);
+};
 console.log(reversedCompanyName);
